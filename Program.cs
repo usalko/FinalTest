1ec28045690e885a2be68691efb7f25b498d3563
Ivan Usalko <ivict@rambler.ru>
2021-12-30 19:44:08 +0300

    # FinalTask
      -  Завершение реализации.

--- Program.cs
+++ Program.cs
@@ -4,14 +4,43 @@ void showHelp()
 {
     string action = inputBuffer.Length > 0 ? "продолжить" : "начать";
     Console.WriteLine($"Нажмите 1 + Enter, для выполнения команды 1: {action} ввод элементов массива");
-    Console.WriteLine("Нажмите 2 + Enter, для выполнения команды 2: завершить ввод элементов массива и вывести результат");
-    Console.WriteLine("Нажмите 3 + Enter, для выполнения команды 3: задать параметры генерации массива случайных чисел и вывести результат");
-    Console.WriteLine("Нажмите 4 + Enter, для выхода из программы");
+    if (inputBuffer.Length > 0)
+    {
+        Console.WriteLine("Нажмите 2 + Enter, для выполнения команды 2: завершить ввод элементов массива и вывести результат");
+        Console.WriteLine("Нажмите 3 + Enter, для выполнения команды 3: очистить массив");
+    }
+    Console.WriteLine("Нажмите 4 + Enter, для выполнения команды 4: задать параметры генерации массива случайных чисел и вывести результат");
+    Console.WriteLine("Нажмите 5 + Enter, для выхода из программы");
 }
 
 int[] filterEvens(int[] array)
 {
     int[] result = { };
+    int[] tmpArray = new int[10];
+    int tal = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            continue;
+        }
+        tmpArray[tal++] = array[i];
+        if (tal >= tmpArray.Length)
+        {
+            int oldSize = result.Length;
+            Array.Resize(ref result, oldSize + tal);
+            Array.ConstrainedCopy(tmpArray, 0, result, oldSize, tal);
+            tal = 0;
+        }
+    }
+
+    if (tal > 0)
+    {
+        int oldSize = result.Length;
+        Array.Resize(ref result, oldSize + tal);
+        Array.ConstrainedCopy(tmpArray, 0, result, oldSize, tal);
+    }
 
     return result;
 }
@@ -64,6 +93,11 @@ void finishInput()
     int[] resultArray = filterEvens(inputBuffer);
     Console.WriteLine("Выходные данные");
     Console.WriteLine(arrayToString(resultArray));
+}
+
+void clearInputArray()
+{
+    inputBuffer = new int[]{};
 }
 
 int getRandomArrayLengthFromInput()
@@ -126,15 +160,19 @@ while (true)
     {
         startInput();
     }
-    else if (nextInput == "2")
+    else if (nextInput == "2" && inputBuffer.Length > 0)
     {
         finishInput();
     }
-    else if (nextInput == "3")
+    else if (nextInput == "3" && inputBuffer.Length > 0)
+    {
+        clearInputArray();
+    }
+    else if (nextInput == "4")
     {
         randomTest();
     }
-    else if (nextInput == "4")
+    else if (nextInput == "5")
     {
         Console.WriteLine("Спасибо за использование приложения");
         break;
